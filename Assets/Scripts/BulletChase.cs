b3d716c19cb563f97b10f05e1ca197b2c535c4a0
JeremiYahya <jeremiyahya777@gmail.com>
2026-07-03 11:23:34 +0700

    Chase sequence 60%

--- Assets/Scripts/BulletChase.cs
+++ Assets/Scripts/BulletChase.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class BulletChase : MonoBehaviour
 {
@@ -7,10 +8,32 @@ public class BulletChase : MonoBehaviour
 
     public void Initialize(Transform t, float s) { target = t; speed = s; }
 
+    void Start()
+    {
+        StartCoroutine(DestroyTimer());
+    }
+
     void Update()
     {
         if (target == null) return;
-        Vector2 dir = (target.position - transform.position).normalized;
+        Vector2 _targetPos = new Vector2(target.position.x, target.position.y - 0.5f);
+        Vector2 _pos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 dir = (_targetPos - _pos).normalized;
         transform.Translate(dir * speed * Time.unscaledDeltaTime); // Unscaled so slow-mo doesn't affect bullet direction
+    }
+
+    IEnumerator DestroyTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            SelfDestruct();
+        }
+    }
+
+    private void SelfDestruct()
+    {
+        StopCoroutine(DestroyTimer());
+        Destroy(this.gameObject);
     }
 }
